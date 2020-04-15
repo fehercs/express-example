c965d61609ed02a6ba95dae552cb216d6437e456
fehercs <feher.csongor@yahoo.com>
2020-04-15 10:08:27 +0200

    fjaspifj

---
+++ pure-http.js
@@ -0,0 +1,7 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello world</h1>');
+  res.end();
+}).listen(8080);
